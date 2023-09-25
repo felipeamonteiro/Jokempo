70d5e6efbbe0e537ecb474ea142199061afd83eb
Felipe <felipeamonteiro.fam@gmail.com>
2023-09-25 14:13:55 -0300

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useState } from 'react';
 import './App.css';
-import Jogador from './components/jogador.jsx';
+import Jogador from './components/jogador';
 import Computador from './components/Computador';
 import Placar from './components/Placar';
 import Resultado from './components/Resultado';
